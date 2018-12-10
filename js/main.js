808ddf80987a81af9f4e0282bd2f67cc47ab5644
vic <741472797@qq.com>
2018-12-10 21:22:46 +0800

    完成会动的简历

--- js/main.js
+++ js/main.js
@@ -1,76 +1,142 @@
-
-var result = `/*
- * 面试官你好，我是钟经义
- * 我将以动画的形式来介绍我自己
- * 只用文字介绍太单调了
- * 我就用代码来介绍吧
- * 首先准备一些样式
- */
-
-*{
-    transition:all 1s;
+/*把code写到#code和style标签里*/
+function writeCss(prefix, code, fn) {
+    let domCode = document.querySelector('#code')
+    let n = 0
+    let id = setInterval(() => {
+        n += 1
+        domCode.innerHTML = Prism.highlight(prefix + code.substring(0, n), Prism.languages.css);
+        styleTag.innerHTML = prefix + code.substring(0, n)
+        domCode.scrollTop = domCode.scrollHeight
+        if (n >= code.length) {
+            window.clearInterval(id)
+            fn && fn.call()
+        }
+    }, 0)
 }
-html{
-    background: rgb(222,222,222);
-    font-size: 16px;
+function writeMarkdown(markdown, fn) {
+    let domPaper = document.querySelector('#paper>.content')
+    let n = 0
+    let id = setInterval(() => {
+        n += 1
+        domPaper.innerHTML = markdown.substring(0, n)
+        domPaper.scrollTop = domPaper.scrollHeight
+        if (n >= markdown.length) {
+            window.clearInterval(id)
+            fn && fn.call()
+        }
+    }, 0)
 }
-#code{
-    border: 1px solid red;
+
+var css1 = `/* 
+   * 面试官你好，我是XXX
+   * 只用文字作做我介绍太单调了
+   * 我就用代码来介绍吧
+   * 首先准备一些样式
+   */
+  *{
+    transition: all 1s;
+  }
+  html{
+    background: #eee;
+  }
+  #code{
+    border: 1px solid #aaa;
     padding: 16px;
-}
-/* 我需要一点代码高亮 */
-.token.selector{
-    color: #690;
-}
-.token.property{
-    color: #905;
-}
-.token.function{
-    color: #DD4A68;
-}
-/*加点3D效果 */
-#code{
-    transform:rotate(360deg);
-}
-/* 不玩了，我来介绍下我自己吧 */
+  }
+  /* 我需要一点代码高亮 */
+  .token.selector{ color: #690; }
+  .token.property{ color: #905; }
+  /* 加一个呼吸效果 */
+  #code{
+    animation: breath 0.5s infinite alternate-reverse;
+  }
+  /* 现在正式开始 */
+  /* 我需要一张白纸 */
+  #code-wrapper{
+    width: 50%; left: 0; position: fixed; 
+    height: 100%;
+  }
+  #paper > .content {
+   display: block;
+  }
+  /* 于是我就可以在白纸上写字了，请看右边 */
+  `
+
+var css2 = `
+  /* 接下来用一个优秀的库 marked.js
+   * 把 Markdown 变成 HTML
+   */
+  `
+var md = `
+# 自我介绍
+我叫 XXX
+1990 年 1 月出生
+XXX 学校毕业
+自学前端半年
+希望应聘前端开发岗位
+
+# 技能介绍
+熟悉 JavaScript CSS
+# 项目介绍
+1. XXX轮播
+2. XXX简历
+3. XXX画板
+
+# 联系方式
+- QQ xxxxxxxx
+- Email xxxxxxxx
+- 手机 xxxxxxx
+
+# 联系方式
+- QQ xxxxxxxx
+- Email xxxxxxxx
+- 手机 xxxxxxx
+
+# 联系方式
+- QQ xxxxxxxx
+- Email xxxxxxxx
+- 手机 xxxxxxx
+
 `
- 
-var n = 0
-var id = setInterval(() => {
-    n += 1
-    code.innerHTML = result.substring(0,n)
-    code.innerHTML = Prism.highlight(code.innerHTML, Prism.languages.css);
-    styleTag.innerHTML = result.substring(0,n)
+let css3 = `
+  /*
+   * 这就是我的会动的简历
+   * 谢谢观看
+   */
+  `
 
-    if(n >= result.length){
-        window.clearInterval(id)
-        fn2()
-        fn3(result)
-    }
-},10)
+writeCss('', css1, () => { // writeCss call the function
+    createPaper(() => {
+        writeMarkdown(md, () => {
+            writeCss(css1, css2, () => {
+                convertMarkdownToHtml(() => {
+                    writeCss(css1 + css2, css3, () => {
+                        console.log('完成')
+                    })
+                })
+            })
+        })
+    })
+})
 
-function fn2(){
+
+
+
+function createPaper(fn) {
     var paper = document.createElement('div')
     paper.id = 'paper'
+    var content = document.createElement('pre')
+    content.className = 'content'
+    paper.appendChild(content)
     document.body.appendChild(paper)
+    fn && fn.call()
 }
-function fn3(preResult){
-    var result = `
-    #paper{
-        width: 100px;height: 100px;
-        background: red;
-    }
-    `
-    var n = 0
-    var id = setInterval(() => {
-        n += 1 
-        code.innerHTML = preResult + result.substring(0,n)
-        code.innerHTML = Prism.highlight(code.innerHTML, Prism.languages.css);
-        styleTag.innerHTML = preResult + result.substring(0,n)
-    
-        if(n >= result.length){
-            window.clearInterval(id)
-        }
-    },50)
- 
+
+function convertMarkdownToHtml(fn) {
+    var div = document.createElement('div')
+    div.className = 'html markdown-body'
+    div.innerHTML = marked(md)
+    let markdownContainer = document.querySelector('#paper > .content')
+    markdownContainer.replaceWith(div)
+    fn && fn.call()
 }
